1b86870f0a749b93a0d271a89c8b881feb1ca37f
qxe <xie.q@sfeir.com>
2016-02-04 15:53:04 +0100

    add director list

--- src/components/movie/MovieDetailsComponent.js
+++ src/components/movie/MovieDetailsComponent.js
@@ -8,22 +8,12 @@ import { Panel } from 'react-bootstrap';
 class MovieDetailsComponent extends React.Component {
   render() {
 
-    // let movieDetail;
-    // debugger;
-    // if(this.props.movie){
-    //   movieDetail = (
-    //     <Panel header={this.props.movie.title}>
-    //       <ul>
-    //         <li><span>Raing: </span>{this.props.movie.rating.average}</li>
-    //         <li><span>Genres: </span>{this.props.movie.genres}</li>
-    //         <li><span>Actors: </span>{this.props.movie.casts}</li>
-    //       </ul>
-    //     </Panel>
-    //   );
-    // }
-
     let genresLst = this.props.movie.genres.map(genre => {
       return (<li>{genre}</li>);
+    });
+
+    let directorLst = this.props.movie.directors.map(director => {
+      return (<li>{director.name}</li>);
     });
 
     let actorsLst = this.props.movie.casts.map(cast => {
@@ -37,6 +27,8 @@ class MovieDetailsComponent extends React.Component {
             <li><span>Raing: </span>{this.props.movie.rating.average}</li>
             <li><span>Genres: </span></li>
             <ul> {genresLst} </ul>
+            <li><span>Directors: </span></li>
+            <ul> {directorLst} </ul>
             <li><span>Actors: </span></li>
             <ul> {actorsLst} </ul>
           </ul>
